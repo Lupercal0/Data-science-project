d128e89c940d5b80129aa780fde8663d903cba40
Lupercal0 <55178796+Lupercal0@users.noreply.github.com>
2022-06-27 15:40:23 +1000

    now have proportion of state to nation
    for every given time(datapoint), there will be a percentage of labour force/nation total in the new csv file.
    data already there, also with the code to generate it, in python

---
+++ code/processing.py
@@ -0,0 +1,43 @@
+import csv
+
+
+csvfile = open('../data/labour_backup.csv', newline='',encoding="utf-8")
+spamreader = csv.DictReader(csvfile)
+
+
+flag = 0
+table_list = []
+total_count = {}
+for row in spamreader:
+    if flag == 0:
+        total_count = row
+        #print(total_count)
+        flag = 2
+        col_name = row.keys()
+        
+        """flag = 1
+        col_name = row
+        #col_name[0] = 'State'
+        print(col_name[0][1])
+        #continue"""
+    
+    else:
+        #print(row)
+        rate_count = {}
+        row_name = ''
+        for key in row.keys():
+            #print(col)
+            if key == 'X':
+                rate_count['X'] = row[key]
+            else:
+                #print(col)
+                rate_count[key] = float(row[key].replace(',',''))/float(total_count[key].replace(',',''))
+        table_list.append(rate_count)
+csvfile.close()
+
+
+with open("../data/rate.csv", 'w', newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=col_name)
+        writer.writeheader()
+        for data in table_list:
+            writer.writerow(data)
