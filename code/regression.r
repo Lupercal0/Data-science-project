a9aa331a1d948dd61a0ad1b6788221462c6f8462
Lupercal0 <55178796+Lupercal0@users.noreply.github.com>
2022-08-16 11:46:46 +1000

    add analysis part for the regression model
    for the aggregation part, using basic regression mode only increase the accuracy limited amount. the major error is still from the forecast part.

--- code/regression.r
+++ code/regression.r
@@ -4,15 +4,42 @@
 #usadata is generated in the visualise.r file
 
 #generate the matrix for process
-y = as.matrix(usadata[52,][2:527])
-y = t(y)
+y = t(as.matrix(usadata[52,][2:527]))
 X = head(usadata, 51)
 X= X[2:527]
 X= matrix(unlist(X),ncol = 51,nrow = 526, byrow = TRUE)
+
 #X be a matrix with states as colnums(parameters) and month's records as rows
 #y is national result with time as row
 
 #basic linear regression
 linear_model = lm(y~X)
 
-# add whatever other regression method you want here
+
+#weighted aggregration based on the regression result
+compare=function(end_year,end_month,forecast, model){
+  xx=as.matrix(forecast)
+  yy=matrix(model$coefficients[2:length(model$coefficients)])
+  intercept = matrix(model$coefficients[1], ncol=1, nrow = 12)
+  result=as.matrix(xx%*%yy)+intercept
+  data=as.numeric(usadata[52,c((2+(end_year-1976)*12+end_month):(2+(end_year-1976)*12+11+end_month))])
+  diff=result-matrix(data)
+  return(diff) 
+}
+
+#weighted aggrgration based on labor force weighted
+compare_real=function(end_year,end_month,forecast){
+  xx=as.matrix(forecast)
+  yy=matrix(percentage[,(2+(end_year-1976)*12+end_month)])
+  result=as.matrix(xx%*%yy)
+  data=as.numeric(usadata[52,c((2+(end_year-1976)*12+end_month):(2+(end_year-1976)*12+11+end_month))])
+  diff=result-matrix(data)
+  return(diff) 
+}
+
+final_diff=compare(2018,12,forecast_res, linear_model)
+plot(final_diff, type="l",main="difference between prediction and real, in 12 month, with regression",xlab="month", ylab="difference in percentage")
+
+final_diff_real=compare_real(2018,12,forecast_res)
+plot(final_diff_real, type="l",main="difference between prediction and real, in 12 month, with labor force rate",xlab="month", ylab="difference in percentage")
+
