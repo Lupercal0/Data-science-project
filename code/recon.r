801bac71fe24d51e5e304379b91d2e9fa2847cb6
Lupercal0 <55178796+Lupercal0@users.noreply.github.com>
2022-09-11 16:29:19 +1000

    reconciliation with comparing
    actually not bad

--- code/recon.r
+++ code/recon.r
@@ -27,17 +27,35 @@ forecast_fun_withnational = function(train_ts, end_year, end_month, period, begi
 }
 
 
-#generate S matrix needed by reconciliation
-
+#generate S matrix needed by reconciliation, which include national as well
 average_weight = t(as.matrix(rowMeans(percentage[-c(1)]), ncol=1))
 filler = matrix(1,1,51)
+#generate summation matrix, to fit the relation. this matrix only represent relation no weight
 S = matrix(0,52,51)
 S[1,]=filler
 for (i in 2:52){
   S[i,i-1] = 1
 }
+#generate G matrix that represent the weight, and GS = I
+for(i in 1:51){
+  temp = matrix(-average_weight[i], 1,52)
+  G[i,] = temp
+  G[i,1] = average_weight[i]
+  G[i,i+1] = 1-average_weight[i]
+}
 
-G = matrix(0, 51, 52)
-G[,1] = t(average_weight)
+prediction = forecast_fun_withnational(usadata, 2018, 12, 12, 39)
+
+recon = S%*%G%*%t(prediction)
 
 
+#print the plot of error , comparing three methods(just weighted sum ,recon and regression result)
+data=as.numeric(usadata[52,c((2+(2018-1976)*12+12):(2+(2018-1976)*12+11+12))])
+diff=recon[1]-matrix(data)
+
+original = forecast_fun(usadata, 12)
+original_weight = compare_real(2018,12,original)
+plot(diff, type="l",main="difference between prediction and real, by reconcilation",xlab="month", ylab="difference in percentage",col="red", ylim = c(0,1.4))
+lines(original_weight, col = "green")
+lines(final_diff, col = "blue")
+legend("topleft", c("with recon", "with easy weight", "with regression"),lty = c(1,1),col = c("red", "green", "blue"))
