d2ca129544f1dd27f43fb03a7708861f67e7d313
Lupercal0 <55178796+Lupercal0@users.noreply.github.com>
2022-09-13 12:00:38 +1000

    write readme file and re form structure
    reorganize what is done and put them together to generate summarize file in the summarize folder. also add explanation to all file in the readme file

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Data-scence-project
 #place for data science project code and data
+library needed for this project(except default R package):
+forecast      (by Rob Hyndman)
+
+
+
+there are 3 folders
+    The file in summarise folder here is mainly about the summarise of work, which include several part:
+        Forecast part include two method:ARIMA and LSTM
+            LSTM method is in the LSTM.ipynb file
+            ARIMA method is in the forecast.r file
+            #should be a file conclude manual-checking of arima parameter here#
+        Aggrefrate part include:
+            reconcilation method is in the recon.r(include special forecas function needed for reconciliation)
+            naive weighted bottom to up method are included in the forecast.r
+            regression method include in recon.r
+    The dada folder:
+        include version of:
+            usa unemployment rate, including national data, 50 states and capital teritory data, as usa_unemployment_nsa.csv
+            processed usa labor force data, as weight, in rate.csv
+    the code folder:
+        include expriment and preprocess data, most has been summarised in to summarise folder
+            adjusting parameterxx-xx.rmd/.r file used to manualy adjust arima parameter
+            analysis.r: use to find the statinoary data
+            docomp.r and decomposition+arima.rmd:used to help find the parameter of arima
+            predict_with_log_transformation.r: use to test the method of log transformation
+            predict.r and predict2.r:use to generate first set of forecast
+            processing.py:used to process labor force data to get weight needed for aggregration
+            regression.r:first test place for the regression method to generate difference.
+            stateCheck.r:file use to check property of timeseise, take alaska as example
+            visualise.r: used to visualise the timeserise itself. 
---
+++ summarise_file/recon.r
@@ -0,0 +1,139 @@
+#same function as the forecast_fun, but reform the structure to invlude the national forecast too, as the first line.
+#same change can be used in the log version too.
+library(forecast)
+path = #your path to usa_unemployment_nsa.csv file
+usadata = read.csv(file = path)
+labour = read.csv(file = path)
+percentage = read.csv(file = path)
+
+
+forecast_fun_withnational = function(train_ts, end_year, end_month, period, begin_month){
+  temp = c(train_ts[52,])
+  temp = temp[2:length(temp)]
+  state_raw = as.vector(unlist(temp))
+  state_data=state_raw[1:(length(state_raw)-begin_month)]
+  ts_state_data=ts(state_data,start=c(1976, 1), end=c(end_year, end_month), frequency=12)
+  #above generate ts of 1976-2018 for each state
+  autoresult=auto.arima(ts_state_data,seasonal = TRUE)
+  prediction = forecast(autoresult, h=period)
+  prediction = t(t(as.vector(prediction$mean[1:length(prediction$mean)])))
+  for(i in 1:51){
+    temp = c(train_ts[i,])
+    temp = temp[2:length(temp)]
+    state_raw = as.vector(unlist(temp))
+    state_data=state_raw[1:(length(state_raw)-begin_month)]
+    ts_state_data=ts(state_data,start=c(1976, 1), end=c(end_year, end_month), frequency=12)
+    #above generate ts of 1976-2018 for each state
+    autoresult=auto.arima(ts_state_data,seasonal = TRUE)
+    fore_res = forecast(autoresult, h=period)
+    fore_res = t(t(as.vector(fore_res$mean[1:length(fore_res$mean)])))
+    prediction = cbind(prediction, fore_res)
+  }
+  return(prediction)
+}
+#to avoid using covid data, whatever period want ot test, please ensuretest dataset ends before 12/2019
+#for both of two forecast function below, 
+#train_ts: is data needed to train and test(assume to be usada here)
+#end_year, end_month: year and month when training set ends(yyyy/mm)
+#period:how many periods you want to forecast, 1 period is 1 month
+#begin_month:how many month are there between end of the trainning set(defined by end_year and end_month), and end of dataset(03/2022), shoud be 27+period
+forecast_fun = function(train_ts, end_year, end_month, period, begin_month){
+  temp = c(train_ts[1,])
+  temp = temp[2:length(temp)]
+  state_raw = as.vector(unlist(temp))
+  state_data=state_raw[1:(length(state_raw)-begin_month)]
+  ts_state_data=ts(state_data,start=c(1976, 1), end=c(end_year, end_month), frequency=12)
+  #above generate ts of 1976-2018 for each state
+  autoresult=auto.arima(ts_state_data,seasonal = TRUE)
+  prediction = forecast(autoresult, h=period)
+  prediction = t(t(as.vector(prediction$mean[1:length(prediction$mean)])))
+  for(i in 2:51){
+    temp = c(train_ts[i,])
+    temp = temp[2:length(temp)]
+    state_raw = as.vector(unlist(temp))
+    state_data=state_raw[1:(length(state_raw)-begin_month)]
+    ts_state_data=ts(state_data,start=c(1976, 1), end=c(end_year, end_month), frequency=12)
+    #above generate ts of 1976-2018 for each state
+    autoresult=auto.arima(ts_state_data,seasonal = TRUE)
+    fore_res = forecast(autoresult, h=period)
+    fore_res = t(t(as.vector(fore_res$mean[1:length(fore_res$mean)])))
+    prediction = cbind(prediction, fore_res)
+  }
+  return(prediction)
+}
+
+#test corelation between states, and try to find alternative ways
+#(mathmetical way rather than theoratical) to find weight.
+
+#usadata is generated in the visualise.r file
+
+#generate the matrix for process
+y = t(as.matrix(usadata[52,][2:527]))
+X = head(usadata, 51)
+X= X[2:527]
+X= matrix(unlist(X),ncol = 51,nrow = 526, byrow = TRUE)
+
+#X be a matrix with states as colnums(parameters) and month's records as rows
+#y is national result with time as row
+
+#basic linear regression
+linear_model = lm(y~X)
+
+#this function assume using usada as real value
+#end_year and end_month: end timepoint of the TRAINNING set
+#forecast: the forecast generate, the period of this should fits the next argument
+#period:how many period(s), in month your forecast are in
+compare=function(end_year,end_month,forecast, period){
+  xx=as.matrix(forecast)
+  yy=matrix(percentage[,(2+(end_year-1976)*12+end_month)])
+  result=as.matrix(xx%*%yy)
+  data=as.numeric(usadata[52,c((2+(end_year-1976)*12+end_month):(2+(end_year-1976)*12+11+period+end_month))])
+  diff=result-matrix(data)
+  return(diff) 
+}
+
+#weighted aggregration based on the regression result
+compare_regre=function(end_year,end_month,forecast, model){
+  xx=as.matrix(forecast)
+  yy=matrix(model$coefficients[2:length(model$coefficients)])
+  intercept = matrix(model$coefficients[1], ncol=1, nrow = 12)
+  result=as.matrix(xx%*%yy)+intercept
+  data=as.numeric(usadata[52,c((2+(end_year-1976)*12+end_month):(2+(end_year-1976)*12+11+end_month))])
+  diff=result-matrix(data)
+  return(diff) 
+}
+
+
+#generate S matrix needed by reconciliation, which include national as well
+average_weight = t(as.matrix(rowMeans(percentage[-c(1)]), ncol=1))
+filler = matrix(1,1,51)
+#generate summation matrix, to fit the relation. this matrix only represent relation no weight
+S = matrix(0,52,51)
+S[1,]=filler
+for (i in 2:52){
+  S[i,i-1] = 1
+}
+#generate G matrix that represent the weight, and GS = I
+for(i in 1:51){
+  temp = matrix(-average_weight[i], 1,52)
+  G[i,] = temp
+  G[i,1] = average_weight[i]
+  G[i,i+1] = 1-average_weight[i]
+}
+
+prediction = forecast_fun_withnational(usadata, 2018, 12, 12, 39)
+
+recon = S%*%G%*%t(prediction)
+final_diff = forecast_fun_withnational(usadata, 2018, 12, 12, 39)
+
+
+#print the plot of error , comparing three methods(just weighted sum ,recon and regression result)
+data=as.numeric(usadata[52,c((2+(2018-1976)*12+12):(2+(2018-1976)*12+11+12))])
+diff=recon[1]-matrix(data)
+
+original = forecast_fun(usadata, 12)
+original_weight = compare_real(2018,12,original)
+plot(diff, type="l",main="difference between prediction and real, by reconcilation",xlab="month", ylab="difference in percentage",col="red", ylim = c(0,1.4))
+lines(original_weight, col = "green")
+lines(final_diff, col = "blue")
+legend("topleft", c("with recon", "with easy weight", "with regression"),lty = c(1,1),col = c("red", "green", "blue"))
