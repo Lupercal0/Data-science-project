9314220b124af6d1742469499c34f3c0e4bf7fe7
Lupercal0 <55178796+Lupercal0@users.noreply.github.com>
2022-10-13 01:59:12 +1100

    fix some problem
    and refine the forecast function

--- code/fresd.r
+++ code/fresd.r
@@ -1,9 +1,51 @@
 #below are caculating
-autoresult_test=auto.arima(ts_national_nc,seasonal = TRUE)
-para = arimaorder(autoresult_test)
-para = append(1, para)
+forecast_with_national= function(train_ts){
+  sigma = c()
+  param = c()
+  temp = c(usadata[52,])
+  temp = temp[2:length(temp)]
+  state_raw = as.vector(unlist(temp))
+  ts_state_data=ts(state_raw,start=c(1976, 1), end=c(year, month), frequency=12)
+  ts_state_data=del_outlier(ts_state_data)
+  
+  fit <- auto.arima(ts_state_data,seasonal = TRUE)
+  #param[1] = arimaorder(fit)
+  sigma=append(sigma, fit$sigma2)
+  ro_pred=forecast(fit,h=12) 
+  ro_res= t(t(as.vector(ro_pred$mean[1:length(ro_pred$mean)])))
+  
+  #  stlf_pred = stlf(ts_state_data,method='arima',h=24)
+  #  stlf_res = t(t(as.vector(stlf_pred$mean[1:length(stlf_pred$mean)])))
+  for(i in 1:51){
+    temp = c(usadata[i,])
+    temp = temp[2:length(temp)]
+    state_raw = as.vector(unlist(temp))
+    ts_state_data=ts(state_raw,start=c(1976, 1), end=c(year, month), frequency=12)
+    ts_state_data=del_outlier(ts_state_data)
+    fit <- Arima(ts_state_data,order=as.numeric(para[i,1:3]),seasonal=as.numeric(para[i,4:6]))
+    param[i] = arimaorder(fit)
+    sigma=append(sigma, fit$sigma2)
+    ro_pred<-forecast(fit,h=12) 
+    ro_pred= t(t(as.vector(ro_pred$mean[1:length(ro_pred$mean)])))
+    ro_res=cbind(ro_res,ro_pred)
+  }
+  res <- list("pred" =ro_res, "sigma" =sigma, 'param' = param)
+  return(res)
+}
 
-psi_vec = ARMAtoMA(ar = para[1], ma = para[2], 12)
+
+forecast_ro=forecast_with_national(usadata)
+
+
+psi_vec  = c()
+
+for(i in 1:51){
+  
+  psi_vec[i] = ARMAtoMA(ar = forecast_ro$param[i][1], ma = forecast_ro$param[i][2], 12)
+}
+#below are caculating
+
+#psi_vec = c(1, ARMAtoMA(ar = para[1], ma = para[2], 12))
 
 
 
@@ -12,7 +54,7 @@ sigp2 = c()
 for(i in 1:12){#also loop on period
   temp = []
   for(j in 1:52){
-    temp[j] = national_variance*sum(para[j][1:i])
+    temp[j] = national_variance*sum(psi_vec[j][1:i])
   }
   sigp2[i] = temp
 }
